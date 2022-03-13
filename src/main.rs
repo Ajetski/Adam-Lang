576ca56d54062a3397d60cae3c85b63e20ff43a6
Adam Jeniski <ajeniski4@gmail.com>
2022-03-13 13:48:03 -0400

    add debug for ci

--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,7 @@ fn compile_and_execute(path: &str) -> i32 {
         .arg(format!("{}/{}.exe", build_folder, target_name))
         .output()
         .unwrap();
+    dbg!(&result);
     println!("result was {}", result.status.code().unwrap());
     result.status.code().unwrap()
 }
